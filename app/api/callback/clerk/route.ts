661884bb6e37542484840934d1ceef23fe250294
takuma-wada <9th.takuma@gmail.com>
2025-05-11 17:27:24 +0900

    :bug: bug fix

--- app/api/callback/clerk/route.ts
+++ app/api/callback/clerk/route.ts
@@ -1,16 +1,46 @@
+import { prisma } from '@/app/lib/prisma'
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
 
 export async function POST(req: NextRequest) {
     try {
         const evt = await verifyWebhook(req)
-
-        // Do something with payload
-        // For this guide, log payload to console
-        const { id } = evt.data
         const eventType = evt.type
-        console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-        console.log('Webhook payload:', evt.data)
+
+        if (eventType === "user.created") {
+            try {
+                await prisma.user.create({
+                    data: {
+                        id: evt.data.id,
+                        name: evt.data.username,
+                        image: evt.data.image_url
+                    },
+                });
+                return new Response("User has been created!", { status: 200 });
+            } catch (err) {
+                console.log(err);
+                return new Response("Filed to create the user!", { status: 500 });
+            }
+        }
+
+        if (eventType === "user.updated") {
+            try {
+                await prisma.user.update({
+                    where: {
+                        id: evt.data.id,
+                    },
+                    data: {
+                        id: evt.data.id,
+                        name: evt.data.username,
+                        image: evt.data.image_url
+                    },
+                });
+                return new Response("User has been updated!", { status: 200 });
+            } catch (err) {
+                console.log(err);
+                return new Response("Filed to update the user!", { status: 500 });
+            }
+        }
 
         return new Response('Webhook received', { status: 200 })
     } catch (err) {
